fa7dcecb9b7ebfb54a6d21bb137531501c52aa19
Hussein <takash.hussein@gmail.com>
2018-07-05 03:21:38 +0400

    Added the user Interface

---
+++ src/components/Credentials/Credentials.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Credentials =() => {
+  return (
+    <div>
+      <div className='f3' style={{display: 'flex', justifyContent: 'flex-end', paddingleft : '10px'}}>
+        {'Developed by Hussein Takch'}
+      </div>
+    </div>
+   );
+}
+
+export default Credentials;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const Rank =() => {
+  return (
+    <div>
+      <div className='white f3'>
+        {'Name, Your current Rank is...'}
+      </div>
+      <div className='white f3'>
+        {'#5'}
+      </div>
+    </div>
+   );
+}
+
+export default Rank;
