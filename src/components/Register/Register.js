a5a952244c44f972dcfe7ebd7a287b12ebe9b51e
Hussein <takash.hussein@gmail.com>
2018-07-05 22:10:02 +0400

    Connected to Heroku

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://young-taiga-63606.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
